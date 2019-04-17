3d5543f5002a402fc311aafacc8587a8a4fc7288
FranciscoArthur <arthurazevedo41@gmail.com>
2019-04-17 18:54:26 -0300

    Update EST56_P1Q3.m

--- EST 56/EST56_P1Q3.m
+++ EST 56/EST56_P1Q3.m
@@ -10,28 +10,31 @@
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 % pop-up que coleta as propriedades do problema
-openfig('EqAnaliseModal.fig');
+
+exemplo = openfig('EqAnaliseModal.fig');
+
 prompt = {'Entre com os valores da Matriz de Massa','m_{1}:','m_{2}:'};
 dlgtitle = 'Matriz de Massa, PARTE 1/5';
 dims = [0 50;1 15;1 15];
-definput = {'','1','2'};
+definput = {'','1','1'};
 opts.Interpreter = 'tex';
 answerMassa = inputdlg(prompt,dlgtitle,dims,definput,opts);
 
 prompt = {'Entre com os valores da Matriz de Rigidez','k_{11}:','k_{12}:','k_{21}:','k_{22}:'};
 dlgtitle = 'Matriz de rigidez, PARTE 2/5';
 dims = [0 50;1 15;1 15;1 15;1 15];
-definput = {'','4','-1','-1','2'};
+definput = {'','2','-1','-1','1'};
 opts.Interpreter = 'tex';
 answerRigidez = inputdlg(prompt,dlgtitle,dims,definput,opts);
 
 prompt = {'Entre com os valores da Matriz de Amortecimento','c_{11}:','c_{12}:','c_{21}:','c_{22}:'};
 dlgtitle = 'Matriz de Amortecimento, PARTE 3/5';
 dims = [0 50;1 15;1 15;1 15;1 15];
-definput = {'','5','-2','-2','3'};
+definput = {'','2','-1','-1','1'};
 opts.Interpreter = 'tex';
 answerAmortecimento = inputdlg(prompt,dlgtitle,dims,definput,opts);
 
+close(exemplo);
 % Matrizes características do problema
 
 M = [str2double(answerMassa{2,1}) 0; 0 str2double(answerMassa{3,1})]
@@ -48,7 +51,7 @@ prompt = {'Entre com as condições iniciais',...
           'Corpo 2','Posição inicial:','Velocidade inicial:'};
 dlgtitle = 'Condições iniciais, PARTE 5/5';
 dims = [0 50;0 30;1 30;1 30;0 30;1 30;1 30];
-definput = {'','','0.2','1.0','','0','0'};
+definput = {'','','0','0','','1','0'};
 answerCondIni = inputdlg(prompt,dlgtitle,dims,definput,opts);
 
 x10 = str2double(answerCondIni{3,1});
@@ -86,13 +89,17 @@ eta0 = [EtaIni(1,1),EtaDotIni(1,1),EtaIni(2,1),EtaDotIni(2,1)]'; %Condições inic
 x = phi*[eta(:,1)';eta(:,3)'];
 
 subplot (211)
-plot (t,x(1,:));
+plot (t,x(1,:),'r');
 xlabel ('t');
 ylabel ('x1 (t)');
+grid on
+title('Deslocamento no tempo para a massa 1');
 subplot (212)
-plot (t,x(2,:));
+plot (t,x(2,:),'k');
 xlabel ('t');
 ylabel ('x2 (t)');
+grid on
+title('Deslocamento no tempo para a massa 2');
 
 % Equacionando as EDOs
 function ydot = Equacionando(t,y,Mcursiv,Kcursiv,Ccursiv,phi)
@@ -104,7 +111,7 @@ Fcursiv_2 = @(t) phi(1,2)*F_1(t) + phi(2,2)*F_2(t);
 
 ydot = zeros(4, 1);
 ydot(1) = y(2);
-ydot(2) = Mcursiv(1,1)^-1*Fcursiv_1(t)-Mcursiv(1,1)^-1*Ccursiv(1,1)*y(2)+Mcursiv(1,1)^-1*Kcursiv(1,1)*y(1);
+ydot(2) = Mcursiv(1,1)^-1*Fcursiv_1(t)-(Mcursiv(1,1)^-1)*Ccursiv(1,1)*y(2)-(Mcursiv(1,1)^-1)*Kcursiv(1,1)*y(1);
 ydot(3) = y(4);
-ydot(4) = Mcursiv(2,2)^-1*Fcursiv_2(t)-Mcursiv(2,2)^-1*Ccursiv(2,2)*y(4)+Mcursiv(2,2)^-1*Kcursiv(2,2)*y(3);
+ydot(4) = Mcursiv(2,2)^-1*Fcursiv_2(t)-(Mcursiv(2,2)^-1)*Ccursiv(2,2)*y(4)-(Mcursiv(2,2)^-1)*Kcursiv(2,2)*y(3);
 end
