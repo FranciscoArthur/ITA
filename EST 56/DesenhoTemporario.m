e974ea882cc054744402d6a93b9df74e50c98526
FranciscoArthur <arthurazevedo41@gmail.com>
2019-04-22 00:43:58 -0300

    Update DesenhoTemporario.m

--- EST 56/DesenhoTemporario.m
+++ EST 56/DesenhoTemporario.m
@@ -2,13 +2,13 @@ m = 1; %kg
 c = 1; 
 k = 1;
 
-m2 = 1.3;
+m2 = 1;
 k2 = 1;
 
 M = [ m 0; 0 m2];
-K = [k+k2 -k2; -k2 k2];
+%K = [k+k2 -k2; -k2 k2];
 C = [c 0; 0 0];
-%K = [2 -1; -1 2];
+K = [2 -1; -1 2];
 F0 = 3;
 
 % Frequências naturais do sistema
@@ -25,12 +25,12 @@ wFor = zeros(1,n);
 Omega = zeros(1,n);
 
 for i = 1: n
-wFor(i) = 3 * (i - 1) / (n-1);
+wFor(i) = 5 * (i - 1) / (n-1);
 Omega(i) = wFor(i)/W1;
 Z = @(r,s) -wFor(i).^2*M(r,s)+1i*wFor(i)*C(r,s)+K(r,s);
 
-X1(i) = (Z(2,2)*F0./(Z(1,1).*Z(2,2)-Z(1,2).^2));
-X2(i) = (-Z(1,2)*F0./(Z(1,1).*Z(2,2)-Z(1,2).^2));
+X1(i) = real(Z(2,2)*F0./(Z(1,1).*Z(2,2)-Z(1,2).^2));
+X2(i) = real(-Z(1,2)*F0./(Z(1,1).*Z(2,2)-Z(1,2).^2));
 
 X0(i) = sqrt((1+(2*Zeta1*Omega(i))^2)/((1-Omega(i)^2)^2+(2*Zeta1*Omega(i))^2));
 end
@@ -39,16 +39,16 @@ end
 
 
 subplot (211);
-plot (wFor/W1d, abs(X1*k/F0),'k');
+plot (wFor/W1d, (X1*k/F0),'k');
 hold on
-plot(wFor/W1d,abs(X0*k/F0),'--r')
+plot(wFor/W1d,(X0*k/F0),'--r')
 plot(1*ones(100,1),linspace(0,max(X1*k/F0)),'b')
 hold off
 xlabel ('w/w_1');
 ylabel ('X_1*K/F_1_0');
 grid on;
 subplot (212);
-plot (wFor/W1d, abs(X2*k/F0),'k');
+plot (wFor/W1d, (X2*k/F0),'k');
 xlabel ('w/w_1');
 ylabel ('X_2*K/F_1_0');
 grid on
