ad8a0c6b08e119984c413164305c5d3ee76fec6c
Arthur's notebook <arthurazevedo41@gmail.com>
2019-04-22 11:35:58 -0300

    Update Lab2.m

--- MVO31/LAB 2/Lab2.m
+++ MVO31/LAB 2/Lab2.m
@@ -1,4 +1,4 @@
-m =  1315;% kg
+W =  1315*9.81;% kg
 s = 16.25;% m2
 Pmax_s = 216253; %W
 
@@ -11,6 +11,28 @@ CD =@(CL) Cd0 + k*CL^2;
 
 CLmax = 2.4;
 CL =@(alfa) 0.02 + 0.12*alfa;
-CM =@(alfa,delta_elev) 0.12 - 0.08alfa + 0.075*delta_elev;
+CM =@(alfa,delta_elev) 0.12 - 0.08*alfa + 0.075*delta_elev;
 nmax = 2;
- 
+Vclimb = 5;%m/s
+
+
+
+
+% item 2
+[~,~,~,rho] = atmosisa(3000);
+V = linspace(1,100);
+Vstall = sqrt(2*W/(rho*s*CLmax));
+Preq =  W*Vclimb +0.5*rho*V.^3*s*Cd0+2*k*W^2./(rho*V.^2*s);
+
+figure
+plot(V,Preq)
+hold on
+plot(V,Pmax_s*ones(1,100))
+plot(Vstall*ones(1,100), linspace(0,max(Preq)))
+
+% item 3
+
+PminGraf = min(Preq)
+VpotMinGraf = interp1(Preq,V,PminGraf)
+%VpotMinTeo = sqrt(-W*)
+%PminTeo = W*Vclimb +0.5*rho*VpotMinTeo.^3*s*Cd0+2*k*W^2./(rho*VpotMinTeo.^2*s)
