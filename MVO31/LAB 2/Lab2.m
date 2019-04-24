fb9d48dec464c741fd5ba03cf458d1b2dbc73878
FranciscoArthur <arthurazevedo41@gmail.com>
2019-04-24 19:21:30 -0300

    Update Lab2.m

--- MVO31/LAB 2/Lab2.m
+++ MVO31/LAB 2/Lab2.m
@@ -3,7 +3,7 @@ s = 16.25;% m2
 Pmax_s = 216253; %W
 
 
-P = @(delt,eta,rho) delt*eta*(rho/1.225)^0.6*Pmax_s; %W
+P = @(delt,eta,rho) delt*eta*(rho./1.225).^0.6*Pmax_s; %W
 Cd0 = 0.026;
 k = 0.054;
 
@@ -78,12 +78,62 @@ alfaVelMin = fsolve(@(alfa) CL(alfa) - CLVelMin,0.5)
 deflexProfundVelMin = fsolve(@(delta)CM(alfaVelMin,delta),0.5)
 
 %% Item 6
-H = 0:5000:20000;
-[~,~,~,rho] = atmosisa(H);
-rho = rho';
+% Variando a altitude
+HVarAlt = 0:5000:20000;
+[~,~,~,rhoVarAlt] = atmosisa(HVarAlt);
+rhoVarAlt = rhoVarAlt';
 Vel = linspace(1,50,1000);
-Preq =  W*Vclimb +0.5*rho*Vel.^3*s*Cd0+2*k*W^2./(rho*Vel*s);
+PreqVarAlt =  W*Vclimb +0.5*rhoVarAlt*Vel.^3*s*Cd0+2*k*W^2./(rhoVarAlt*Vel*s);
 
 figure
-plot(Vel,Preq)
+plot(Vel,PreqVarAlt)
 grid on
+legend('Altitude 0 m', 'Altitude 5000 m', 'Altitude 10000 m', 'Altitude 15000 m', 'Altitude 20000 m');
+
+% Variando a Vclimb
+HVarRazSub = 3000;
+[~,~,~,rhoVarRazSub] = atmosisa(HVarRazSub);
+Vel = linspace(1,50,1000);
+VclimbVarRazSub = (0:50:200)';
+PreqVarRazSub =  W*VclimbVarRazSub +0.5*rhoVarRazSub*Vel.^3*s*Cd0+2*k*W^2./(rhoVarRazSub*Vel*s);
+
+figure
+plot(Vel,PreqVarRazSub)
+grid on
+legend('Razão de subida 0 m/s', 'Razão de subida 50 m/s', 'Razão de subida 100 m/s', 'Razão de subida 150 m/s', 'Razão de subida 200 m/s');
+
+%% Item 7
+
+[~,~,~,rho] = atmosisa(3000);
+VVelClimbMax = sqrt((2*W/(rho*s))*sqrt(k/(3*Cd0)))
+VclimbMax = P(1,eta,rho)/W - (4/3)*sqrt(2*W/(rho*s)*sqrt(3*k^3*Cd0))
+
+% Angulo de ataque
+CLVclimbMax = W/(0.5*rho*VVelClimbMax^2*s)
+alfaVclimbMax = fsolve(@(alfa) CL(alfa) - CLVclimbMax,0.5)
+
+% Deflexão de profundor
+deflexProfundVclimbMax = fsolve(@(delta)CM(alfaVclimbMax,delta),0.5)
+
+%% Item 8
+VclimbTeto = 5;
+
+TetoVoo = @(rhoTeto) W*VclimbTeto + 2*W*sqrt(k*Cd0) - P(1,eta,rhoTeto); % corrigir
+
+rhoTeto = fsolve(TetoVoo, 0.8);
+
+HtetoAux = 0:5:20000;
+[~,~,~,rhoTetoAux] = atmosisa(HtetoAux);
+rhoTetoAuxPosic = find(abs(rhoTetoAux - rhoTeto) < 10^-4);
+Hteto = HtetoAux(rhoTetoAuxPosic(1,1))
+clear HtetoAux rhoTetoAux rhoTetoAuxPosic
+
+%% Item 9
+HEnvel = linspace(0,80000,10000);
+Vclimb = 5;
+[~,~,~,rhoEnvel] = atmosisa(HEnvel);
+
+PoliVreq = [0.5*rhoEnvel*s*Cd0 0 0 (W*Vclimb - P(1,eta,rhoEnvel)) 2*k*W^2./(rhoEnvel*s)];
+
+Vreq = sort(roots(PoliVreq));
+Vreq = Vreq(1,1)
