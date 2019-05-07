9857e1c41df066d96c753bbe0d844683d3cd1aea
FranciscoArthur <arthurazevedo41@gmail.com>
2019-05-07 12:53:26 -0300

    Update deriveRobotDynamicEquations.m

--- EE 254/MPC SS/deriveRobotDynamicEquations.m
+++ EE 254/MPC SS/deriveRobotDynamicEquations.m
@@ -14,8 +14,8 @@ M = [col1, col2, col3];
 
 Mpinv = (M' * M)^-1 * M';
 vr = r * Mpinv * [w1; w2; w3; w4];
- cm = [xcm; ycm; 0];
-%cm = [0; 0; 0];
+cm = [xcm; ycm; 0];
+
 vcm = [vr(1); vr(2); 0] + cross([0; 0; vr(3)], cm);
 w = vr(3);
 
@@ -38,7 +38,38 @@ H4 = subs(dLdw, [w1 w2 w3 w4], [0 0 0 1]);
 
 H = [H1, H2, H3, H4]
 
-Id = subs(dLdw1, [w1 w2 w3 w4], [1 0 0 0])
+
+%% atualizar
+
+sysName = 'my_ODE';
+Q_e = {tal1 tal2 tal3 tal4}; %forças externas
+par = {m r l ycm xcm Icm}; %parametros
+VF = ?;
+
+m_fileName  = strcat(sysName,'.m');
+s_fileName  = strcat(sysName,'_block');
+s_blockName = strcat(s_fileName,'/',sysName);
+
+Q_vars = Q_e(cellfun('isclass', Q_e, 'sym'));
+Q_vars = subs(Q_vars,X2n,Xt2n);
+
+% create MATLAB function
+matlabFunction(VF,...
+    'file',     m_fileName,...
+    'vars',   [{'t','Y'}, Q_vars, par],...
+    'outputs', {'dYdt'});
+% create Simulink block
+new_system  (s_fileName)
+matlabFunctionBlock(s_blockName,VF,...
+    'functionName', sysName,...
+    'vars',       [{'t','Y'}, Q_vars, par],...
+    'outputs',     {'dYdt'});
+save_system (s_fileName)
+close_system(s_fileName)
+
+
+
+% Id = subs(dLdw1, [w1 w2 w3 w4], [1 0 0 0])
 % Iwc = subs(dLdw1, [w1 w2 w3 w4], [0 1 0 0])
 % Isc = subs(dLdw1, [w1 w2 w3 w4], [0 0 1 0])
 
