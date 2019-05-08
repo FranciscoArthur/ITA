350dd92d0672b9e24171b78f425c3f391c7aa134
FranciscoArthur <arthurazevedo41@gmail.com>
2019-05-07 23:42:44 -0300

    Update deriveRobotDynamicEquations.m

--- EE 254/MPC SS/deriveRobotDynamicEquations.m
+++ EE 254/MPC SS/deriveRobotDynamicEquations.m
@@ -1,6 +1,6 @@
-function deriveRobotDynamicEquations()
 
 syms w1 w2 w3 w4 r l N m Icm xcm ycm real
+syms Input
 
 % alpha = deg2rad([33; 147; 225; 315]); % Skuba
 alpha = deg2rad([45; 135; 225; 315]); % ITAndroids
@@ -25,10 +25,10 @@ V = 0;
 
 L = T - V;
 
-dLdw1 = expand(simplify(diff(L, w1)))
-dLdw2 = expand(simplify(diff(L, w2)))
-dLdw3 = expand(simplify(diff(L, w3)))
-dLdw4 = expand(simplify(diff(L, w4)))
+dLdw1 = expand(simplify(diff(L, w1)));
+dLdw2 = expand(simplify(diff(L, w2)));
+dLdw3 = expand(simplify(diff(L, w3)));
+dLdw4 = expand(simplify(diff(L, w4)));
 
 dLdw = [dLdw1; dLdw2; dLdw3; dLdw4];
 H1 = subs(dLdw, [w1 w2 w3 w4], [1 0 0 0]);
@@ -36,41 +36,30 @@ H2 = subs(dLdw, [w1 w2 w3 w4], [0 1 0 0]);
 H3 = subs(dLdw, [w1 w2 w3 w4], [0 0 1 0]);
 H4 = subs(dLdw, [w1 w2 w3 w4], [0 0 0 1]);
 
-H = [H1, H2, H3, H4]
+H = simplify([H1, H2, H3, H4]);
 
 
 %% atualizar
 
-sysName = 'my_ODE';
-Q_e = {tal1 tal2 tal3 tal4}; %forças externas
+sysName = 'RobotDynamicMatrixH';
 par = {m r l ycm xcm Icm}; %parametros
-VF = ?;
+Output = H+Input;
 
 m_fileName  = strcat(sysName,'.m');
 s_fileName  = strcat(sysName,'_block');
 s_blockName = strcat(s_fileName,'/',sysName);
 
-Q_vars = Q_e(cellfun('isclass', Q_e, 'sym'));
-Q_vars = subs(Q_vars,X2n,Xt2n);
-
 % create MATLAB function
-matlabFunction(VF,...
+matlabFunction(Output,...
     'file',     m_fileName,...
-    'vars',   [{'t','Y'}, Q_vars, par],...
-    'outputs', {'dYdt'});
+    'vars',   [ Input, par],...
+    'outputs', {'InputPlusH'});
 % create Simulink block
 new_system  (s_fileName)
-matlabFunctionBlock(s_blockName,VF,...
+matlabFunctionBlock(s_blockName,Output,...
     'functionName', sysName,...
-    'vars',       [{'t','Y'}, Q_vars, par],...
-    'outputs',     {'dYdt'});
+    'vars',       [Input, par],...
+    'outputs',     {'InputPlusH'});
 save_system (s_fileName)
 close_system(s_fileName)
 
-
-
-% Id = subs(dLdw1, [w1 w2 w3 w4], [1 0 0 0])
-% Iwc = subs(dLdw1, [w1 w2 w3 w4], [0 1 0 0])
-% Isc = subs(dLdw1, [w1 w2 w3 w4], [0 0 1 0])
-
-end
