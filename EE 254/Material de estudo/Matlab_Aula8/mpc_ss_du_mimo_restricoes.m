d770c8cb7cd526d5ddce45c593136bebf47b0c97
FranciscoArthur <arthurazevedo41@gmail.com>
2019-05-10 16:53:34 -0300

    Update mpc_ss_du_mimo_restricoes.m

--- EE 254/Material de estudo/Matlab_Aula8/mpc_ss_du_mimo_restricoes.m
+++ EE 254/Material de estudo/Matlab_Aula8/mpc_ss_du_mimo_restricoes.m
@@ -46,7 +46,7 @@ bqp = [repmat(dumax,M,1); -repmat(dumin,M,1); repmat((umax - uk1),M,1); repmat((
 
 fqp = 2*Gn'*(f - r);
 
-options = optimset('Algorithm','active-set','Display','final');
+options = optimset('Algorithm','interior-point-convex','Display','final');
 DUk = quadprog(Hqp,fqp,Aqp,bqp,[],[],[],[],[],options);
 
 sys =  DUk(1:p); % Saída da S-function (incremento nos p controles)
