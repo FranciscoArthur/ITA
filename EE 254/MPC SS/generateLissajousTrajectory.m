3bb729ab58368e8025cd540ba80b8b854f514d0d
Arthur's notebook <arthurazevedo41@gmail.com>
2019-05-13 21:14:09 -0300

    Update generateLissajousTrajectory.m

--- EE 254/MPC SS/generateLissajousTrajectory.m
+++ EE 254/MPC SS/generateLissajousTrajectory.m
@@ -1,18 +1,25 @@
 function X = generateLissajousTrajectory(A,B,a,b,tspan)
 
-t = linspace(0,tspan,300);
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%     Função que gera os pontos de uma trajetória que segue as funções
+%  de Lissajous. Feita para simulações do robô Small Size, necessita do
+%  tempo de simulação e calcula o ângulo do robô com a trajetória.
+%  
+%     Feito em 13/05/2019
+%     Autor: Arthur Azevedo
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
-x = A*sin(a*t);
-y = B*sin(b*t);
+t = linspace(0,tspan,300); %tempo total da trajetória
+
+x = A*sin(a*t); % trajetória paramétrica em x
+y = B*sin(b*t); % trajetória paramétrica em y
 
 
-dx = A * a * sin(a * t);
-dy = B * b * sin(b * t);
+dx = A * a * cos(a * t); % derivada de x
+dy = B * b * cos(b * t); % derivada de y
 
-theta = atan2(dy./dx);
+theta = atan2(dy,dx)-pi/2; % ângulo que o robô (SS) faz com a trajetória
 
 X = [x',y',theta'];
-
-%plot(x,y)
 
 end
