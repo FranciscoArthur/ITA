93a380238613381edb19602c8449219fa684a742
FranciscoArthur <arthurazevedo41@gmail.com>
2019-07-11 22:37:11 -0300

    Create traceInfo_flag.js

---
+++ ICs/IC Geneva Drive SSL codigos/untitled_ert_rtw/html/traceInfo_flag.js
@@ -0,0 +1,16 @@
+function TraceInfoFlag() {
+    this.traceFlag = new Array();
+    this.traceFlag["untitled.c:155c19"]=1;
+    this.traceFlag["untitled.c:155c30"]=1;
+}
+top.TraceInfoFlag.instance = new TraceInfoFlag();
+function TraceInfoLineFlag() {
+    this.lineTraceFlag = new Array();
+    this.lineTraceFlag["untitled.c:150"]=1;
+    this.lineTraceFlag["untitled.c:155"]=1;
+    this.lineTraceFlag["untitled.c:187"]=1;
+    this.lineTraceFlag["untitled.c:190"]=1;
+    this.lineTraceFlag["untitled.c:227"]=1;
+    this.lineTraceFlag["untitled.c:230"]=1;
+}
+top.TraceInfoLineFlag.instance = new TraceInfoLineFlag();
