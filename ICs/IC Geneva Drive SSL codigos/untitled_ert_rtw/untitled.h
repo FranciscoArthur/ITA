628b7dfa45318497de45b8fd2b9ea12813962de0
FranciscoArthur <arthurazevedo41@gmail.com>
2019-07-11 22:38:13 -0300

    Create untitled.h

---
+++ ICs/IC Geneva Drive SSL codigos/untitled_ert_rtw/untitled.h
@@ -0,0 +1,183 @@
+/*
+ * Sponsored License - for use in support of a program or activity
+ * sponsored by MathWorks.  Not for government, commercial or other
+ * non-sponsored organizational use.
+ *
+ * File: untitled.h
+ *
+ * Code generated for Simulink model 'untitled'.
+ *
+ * Model version                  : 1.0
+ * Simulink Coder version         : 8.12 (R2017a) 16-Feb-2017
+ * C/C++ source code generated on : Wed Aug 29 13:11:40 2018
+ *
+ * Target selection: ert.tlc
+ * Embedded hardware selection: Intel->x86-64 (Windows64)
+ * Code generation objectives:
+ *    1. RAM efficiency
+ *    2. Execution efficiency
+ * Validation result: Not run
+ */
+
+#ifndef RTW_HEADER_untitled_h_
+#define RTW_HEADER_untitled_h_
+#include <string.h>
+#ifndef untitled_COMMON_INCLUDES_
+# define untitled_COMMON_INCLUDES_
+#include "rtwtypes.h"
+#include "rtw_continuous.h"
+#include "rtw_solver.h"
+#endif                                 /* untitled_COMMON_INCLUDES_ */
+
+/* Macros for accessing real-time model data structure */
+#ifndef rtmGetErrorStatus
+# define rtmGetErrorStatus(rtm)        ((rtm)->errorStatus)
+#endif
+
+#ifndef rtmSetErrorStatus
+# define rtmSetErrorStatus(rtm, val)   ((rtm)->errorStatus = (val))
+#endif
+
+#ifndef rtmGetStopRequested
+# define rtmGetStopRequested(rtm)      ((rtm)->Timing.stopRequestedFlag)
+#endif
+
+#ifndef rtmSetStopRequested
+# define rtmSetStopRequested(rtm, val) ((rtm)->Timing.stopRequestedFlag = (val))
+#endif
+
+#ifndef rtmGetStopRequestedPtr
+# define rtmGetStopRequestedPtr(rtm)   (&((rtm)->Timing.stopRequestedFlag))
+#endif
+
+#ifndef rtmGetT
+# define rtmGetT(rtm)                  (rtmGetTPtr((rtm))[0])
+#endif
+
+/* Forward declaration for rtModel */
+typedef struct tag_RTM RT_MODEL;
+
+/* Block signals and states (auto storage) for system '<Root>' */
+typedef struct {
+  real_T Integrator;                   /* '<Root>/Integrator' */
+  real_T Sum;                          /* '<Root>/Sum' */
+} DW;
+
+/* Continuous states (auto storage) */
+typedef struct {
+  real_T g;                            /* '<Root>/Integrator' */
+  real_T g_j;                          /* '<Root>/Integrator1' */
+} X;
+
+/* State derivatives (auto storage) */
+typedef struct {
+  real_T g;                            /* '<Root>/Integrator' */
+  real_T g_j;                          /* '<Root>/Integrator1' */
+} XDot;
+
+/* State disabled  */
+typedef struct {
+  boolean_T g;                         /* '<Root>/Integrator' */
+  boolean_T g_j;                       /* '<Root>/Integrator1' */
+} XDis;
+
+#ifndef ODE3_INTG
+#define ODE3_INTG
+
+/* ODE3 Integration Data */
+typedef struct {
+  real_T *y;                           /* output */
+  real_T *f[3];                        /* derivatives */
+} ODE3_IntgData;
+
+#endif
+
+/* Real-time Model Data Structure */
+struct tag_RTM {
+  const char_T *errorStatus;
+  RTWSolverInfo solverInfo;
+  X *contStates;
+  int_T *periodicContStateIndices;
+  real_T *periodicContStateRanges;
+  real_T *derivs;
+  boolean_T *contStateDisabled;
+  boolean_T zCCacheNeedsReset;
+  boolean_T derivCacheNeedsReset;
+  boolean_T blkStateChange;
+  real_T odeY[2];
+  real_T odeF[3][2];
+  ODE3_IntgData intgData;
+
+  /*
+   * Sizes:
+   * The following substructure contains sizes information
+   * for many of the model attributes such as inputs, outputs,
+   * dwork, sample times, etc.
+   */
+  struct {
+    int_T numContStates;
+    int_T numPeriodicContStates;
+    int_T numSampTimes;
+  } Sizes;
+
+  /*
+   * Timing:
+   * The following substructure contains information regarding
+   * the timing information for the model.
+   */
+  struct {
+    uint32_T clockTick0;
+    time_T stepSize0;
+    uint32_T clockTick1;
+    SimTimeStep simTimeStep;
+    boolean_T stopRequestedFlag;
+    time_T *t;
+    time_T tArray[2];
+  } Timing;
+};
+
+/* Continuous states (auto storage) */
+extern X rtX;
+
+/* Block signals and states (auto storage) */
+extern DW rtDW;
+
+/* Model entry point functions */
+extern void untitled_initialize(void);
+extern void untitled_step(void);
+
+/* Real-time Model object */
+extern RT_MODEL *const rtM;
+
+/*-
+ * These blocks were eliminated from the model due to optimizations:
+ *
+ * Block '<Root>/Scope' : Unused code path elimination
+ * Block '<Root>/Gain' : Eliminated nontunable gain of 1
+ * Block '<Root>/Gain1' : Eliminated nontunable gain of 1
+ * Block '<Root>/Gain2' : Eliminated nontunable gain of 1
+ */
+
+/*-
+ * The generated code includes comments that allow you to trace directly
+ * back to the appropriate location in the model.  The basic format
+ * is <system>/block_name, where system is the system number (uniquely
+ * assigned by Simulink) and block_name is the name of the block.
+ *
+ * Use the MATLAB hilite_system command to trace the generated code back
+ * to the model.  For example,
+ *
+ * hilite_system('<S3>')    - opens system 3
+ * hilite_system('<S3>/Kp') - opens and selects block Kp which resides in S3
+ *
+ * Here is the system hierarchy for this model
+ *
+ * '<Root>' : 'untitled'
+ */
+#endif                                 /* RTW_HEADER_untitled_h_ */
+
+/*
+ * File trailer for generated code.
+ *
+ * [EOF]
+ */
